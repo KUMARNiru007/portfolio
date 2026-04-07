4ec61e733af7afb5afb21c50cafcccde59959964
Kumar Nirupam <kumar.nirupam24@gmail.com>
2026-04-07 11:43:50 +0530

    fix-2

--- src/components/TechStackMarquee.tsx
+++ src/components/TechStackMarquee.tsx
@@ -91,7 +91,7 @@ const TechStackMarquee: React.FC = () => {
 
           {/* Fade edges */}
           <div className="pointer-events-none absolute inset-y-0 left-0 w-20 bg-gradient-to-r from-background to-transparent" />
-          <div className="pointer-events-none absolute inset-y-0 right-0 w-20 bg-gradient-to-l from-background to-transparent"/>
+          <div className="pointer-events-none absolute inset-y-0 right-0 w-20  bg-gradient-to-l from-background to-transparent"/>
         </div>
       </Container>
     </Reveal>
